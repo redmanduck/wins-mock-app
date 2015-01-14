5e1fab2284f5e85ad655b355a103c8ab8fc0b35c
Pat Sabpisal <ssabpisa@me.com>
2015-01-14 16:03:00 -0500

    Initial Commit - with scanning

---
+++ app/src/main/java/edu/purdue/ece477/triangulationtest/MainActivity.java
@@ -0,0 +1,59 @@
+package edu.purdue.ece477.triangulationtest;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private WifiManager mWifiManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get wifi manager
+        mWifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+
+        WifiInfo current = mWifiManager.getConnectionInfo();
+
+        List<ScanResult> other = mWifiManager.getScanResults();
+        for(ScanResult sr : other){
+            // BSSID is a function of MAC address
+            System.out.println( sr.BSSID  + " = " + sr.level);
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
